acbbca1bfa2627e5cd3b69e0de25f09fcb9190ef
LEGALOUDECFlorian <pti.breton@hotmail.fr>
2025-03-19 14:58:25 +0100

    fix title typo

--- where_s_fethi_going/src/app/app.component.ts
+++ where_s_fethi_going/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { MapCaseComponent } from './components/map-case.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'where_s_fethi_going';
+  title = 'where is Fethi ?';
 }
